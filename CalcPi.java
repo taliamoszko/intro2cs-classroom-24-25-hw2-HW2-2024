7f6e82ebf344f375acdde986d975c05fa524fe51
Tal Danai <70094025+danaital@users.noreply.github.com>
2024-11-11 13:23:16 +0200

    Update CalcPi.java

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,6 @@
 // Computes an approximation of PI.
 public class CalcPi {
-    public static void main(String args[]) { 
+	public static void main(String [] args) { 
 		// Replace this comment with your code
-	}
+    	}
 }
